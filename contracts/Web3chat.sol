2b1bae468e8f2416d586b25e34f543c25de3c432
Paul Simroth <paul.simroth@proton.me>
2023-05-17 18:29:55 +0200

    erc721 import

--- contracts/Web3chat.sol
+++ contracts/Web3chat.sol
@@ -1,4 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Web3chat {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Web3chat is ERC721 {
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        
+    }
+}
