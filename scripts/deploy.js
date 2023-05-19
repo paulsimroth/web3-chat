93e2176395dcdcc678e1a9e7b2db71799832c9df
Paul Simroth <paul.simroth@proton.me>
2023-05-19 11:49:26 +0200

    deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,8 +5,29 @@ const tokens = (n) => {
 }
 
 async function main() {
+  //Setting accounts and constructor variables
+  const [deployer] = await ethers.getSigners();
+  const NAME = "Web3chat";
+  const SYMBOL = "W3C";
 
-}
+  //Deploying contract
+  const Web3chat = await ethers.getContractFactory("Web3chat");
+  const web3chat = await Web3chat.deploy(NAME, SYMBOL);
+  await web3chat.deployed();
+
+  console.log(`Contract deployed! ADDRESS: ${web3chat.address}\n`);
+
+  //Create 3 Channels
+  const CHANNEL_NAMES = ["general", "announcements", "intro"];
+  const COSTS = [tokens(0), tokens(1), tokens(0.25)];
+
+  for (let i = 0; i < 3; i++) {
+    const tx = await web3chat.connect(deployer).createChannel(CHANNEL_NAMES[i], COSTS[i]);
+    await tx.wait();
+
+    console.log(`Created Channel #${CHANNEL_NAMES[i]}\n`);
+  };
+};
 
 main().catch((error) => {
   console.error(error);
